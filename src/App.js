42821cb11ff5bd3d176cf04dcf25a54d0edd7ff2
Jaggehn <jaggehns@gmail.com>
2021-04-26 17:25:16 +0800

    Set up and deploy with firebase

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Todo from "./Todo";
 import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
 import "./App.css";
+import db from "./firebase";
+import firebase from "firebase";
 
 function App() {
-  const [todos, setTodos] = useState([
-    "Take dogs for a walk",
-    "Take the rubbish out",
-    "Take the rubbish out ok",
-  ]);
-
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
+
+  //when the app loads, we need to listen to the database and fetch new todos as they get added/removed
+  useEffect(() => {
+    //this code here fires whent the app loads
+    db.collection("todos")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setTodos(snapshot.docs.map((doc) => doc.data().todo));
+      });
+  }, []);
 
   const addTodo = (event) => {
     //this will fire off when we click the button
     event.preventDefault(); //Stop the refresh
-    setTodos([...todos, input]);
+
+    db.collection("todos").add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
     setInput(""); //Clear input
   };
 
@@ -24,7 +36,7 @@ function App() {
       <h1>Hello World!</h1>
       <form>
         <FormControl>
-          <InputLabel>Write a Todo</InputLabel>
+          <InputLabel>✅ Write a Todo</InputLabel>
           <Input
             value={input}
             onChange={(event) => setInput(event.target.value)}
@@ -43,7 +55,6 @@ function App() {
       <ul>
         {todos.map((todo) => (
           <Todo text={todo} />
-          // <li>{todo}</li>
         ))}
       </ul>
     </div>
