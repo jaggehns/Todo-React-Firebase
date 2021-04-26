63cedae8d678d6ff0170ff856732d7c86aa634fe
Jaggehn <jaggehns@gmail.com>
2021-04-26 21:07:19 +0800

    Added delete functionality and redeployed

--- src/Todo.js
+++ src/Todo.js
@@ -2,19 +2,25 @@ import React from "react";
 import "./Todo.css";
 import {
   List,
-  Avatar,
   ListItem,
   ListItemText,
   ListItemAvatar,
 } from "@material-ui/core";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import db from "./firebase";
 
 function Todo(props) {
   return (
     <List className="todo__list">
       <ListItem>
         <ListItemAvatar></ListItemAvatar>
-        <ListItemText primary={props.text} secondary="Dummy Deadline" />
+        <ListItemText primary={props.todo.todo} secondary="Dummy Deadline ⏰" />
       </ListItem>
+      <DeleteForeverIcon
+        onClick={(event) => db.collection("todos").doc(props.todo.id).delete()}
+      >
+        🗑️ DELETE ME
+      </DeleteForeverIcon>
     </List>
   );
 }
